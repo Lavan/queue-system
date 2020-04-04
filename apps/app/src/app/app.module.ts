00b44485a9d4db7993cb6d4a0fc0b61ead60f635
Mattias Flodin <git@codepentry.com>
2020-04-04 19:30:15 +0200

    Position indicator for place in line

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { appRoutes } from './app.routing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SiteComponent } from './site/site.component';
 import { MatInputModule } from '@angular/material/input';
@@ -25,7 +26,7 @@ import { SiteService } from './services/site.service';
   declarations: [AppComponent, SiteQueueDetailComponent, SiteDetailComponent, QueueDetailComponent, EnterComponent, TicketComponent, RootComponent, SiteComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
-    MatToolbarModule, MatIconModule, MatButtonModule,
+    MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule,
     FlexLayoutModule, MatInputModule, FormsModule, ReactiveFormsModule],
   providers: [SiteService],
   bootstrap: [AppComponent]
