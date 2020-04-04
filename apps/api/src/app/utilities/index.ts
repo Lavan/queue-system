e878b9127419313826888173fb30f56e15f72598
per <735079+Lavan@users.noreply.github.com>
2020-04-04 22:02:25 +0200

    Fixed generateRandomString

--- apps/api/src/app/utilities/index.ts
+++ apps/api/src/app/utilities/index.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto';
 
 export function generateRandomString(length: number = 24) {
-  return crypto.randomBytes(Math.floor((length + 3) / 4) * 3).toString('base64');
+  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex');
 }
 
 /**
