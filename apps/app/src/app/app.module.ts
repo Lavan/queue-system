ca51bbc0dbb6891e5df1c4fdd4272a47a2c1ddef
Mattias Flodin <git@codepentry.com>
2020-04-04 12:44:39 +0200

    "Enter" component

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SiteQueueDetailComponent } from './site-queue-detail/site-queue-detail.component';
 import { SiteDetailComponent } from './site-detail/site-detail.component';
 import { QueueDetailComponent } from './queue-detail/queue-detail.component';
+import { EnterComponent } from './enter/enter.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { RootComponent } from './root/root.component';
 import { RouterModule } from '@angular/router';
@@ -17,7 +18,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [AppComponent, SiteQueueDetailComponent, SiteDetailComponent, QueueDetailComponent, TicketComponent, RootComponent],
+  declarations: [AppComponent, SiteQueueDetailComponent, SiteDetailComponent, QueueDetailComponent, EnterComponent, TicketComponent, RootComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
     MatToolbarModule, MatIconModule, MatButtonModule,
--- apps/app/src/app/app.routing.ts
+++ apps/app/src/app/app.routing.ts
@@ -4,8 +4,10 @@ import { SiteDetailComponent } from './site-detail/site-detail.component';
 import { QueueDetailComponent } from './queue-detail/queue-detail.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { RootComponent } from './root/root.component';
+import { EnterComponent } from './enter/enter.component';
 
 export const appRoutes: Routes = [
+  { path: ':queueId/enter', component: EnterComponent },
   { path: 'site/:siteId/:queueId', component: SiteQueueDetailComponent },
   { path: 'site/:siteId', component: SiteDetailComponent },
   { path: 'queue/:queueId', component: QueueDetailComponent },
